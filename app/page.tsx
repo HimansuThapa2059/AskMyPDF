2cbaf473bf81829a0667b78abf26e8cd8531961f
Himansu <himansuthapa2059@gmail.com>
2024-10-18 22:21:19 +0545

    add landing page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,172 @@
+import Link from 'next/link';
+import { MoveRight } from 'lucide-react';
+import { buttonVariants } from '@/components/ui/button';
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className="max-container mb-12 mt-28 sm:mt-40 flex flex-col justify-center items-center text-center">
-      <div className="mx-auto mb-4 max-w-fit flex items-center justify-center  rounded-full space-x-2 overflow-hidden border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-        <p className="text-sm font-semibold text-gray-700">
-          AskMyPDF is available now!
+    <>
+      <div className="max-container mb-12 mt-28 sm:mt-40 flex flex-col justify-center items-center text-center">
+        <div className="cursor-pointer mx-auto mb-4 max-w-fit flex items-center justify-center  rounded-full space-x-2 overflow-hidden border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <div className="text-sm font-semibold text-gray-700 flex items-center justify-between">
+            <Image
+              src={'/ai-stars.png'}
+              alt=""
+              height={18}
+              width={18}
+              className="dark:filter dark:invert"
+            />
+            <span>AskMyPDF is available now!</span>
+          </div>
+        </div>
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          Chat with your <span className="text-indigo-600">documents</span> in
+          seconds.
+        </h1>
+        <p className="mt-5 max-w-prose text-zinc-700  sm:text-lg">
+          <span className="text-indigo-600">AskMyPDF</span> allows you to have
+          conversations with any PDF document. Simply upload your file and start
+          asking questions right away.
         </p>
+        <Link
+          className={buttonVariants({
+            size: 'lg',
+            className: 'mt-5 bg-indigo-950',
+          })}
+          href="/dashboard"
+          target="_blank"
+        >
+          Get started <MoveRight className="ml-1 h-5 w-5" />
+        </Link>
       </div>
-    </div>
+
+      <div>
+        <div className="relative isolate">
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+
+          <div>
+            <div className="mx-auto max-w-6xl px-10 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/25 p-0.5 md:p-1 ring-1 ring-inset ring-gray-900/20 lg:-m-4  ">
+                  <Image
+                    src="/dashboard-preview.jpg"
+                    alt="product preview"
+                    width={1364}
+                    height={866}
+                    quality={100}
+                    className="rounded-[10px] md:rounded-[9px] bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/20"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-auto mb-32 mt-28 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              Start chatting in minutes
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              Chatting to your PDF files has never been easier than with
+              AskMyPDF.
+            </p>
+          </div>
+        </div>
+
+        {/* steps */}
+        <ol className="my-8 space-y-4 p-4 md:px-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4 md:items-center">
+              <span className="text-sm font-semibold text-indigo-600">
+                Step 1
+              </span>
+              <span className="text-xl font-semibold ">
+                Sign up for an account
+              </span>
+              <span className="mt-2 text-zinc-700 md:text-center">
+                Either starting out with a free plan or choose our{' '}
+                <Link
+                  href="/pricing"
+                  className="text-indigo-700 underline underline-offset-2"
+                >
+                  pro plan
+                </Link>
+                .
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4 md:items-center">
+              <span className="text-sm font-semibold text-indigo-600">
+                Step 2
+              </span>
+              <span className="text-xl font-semibold ">
+                Upload your PDF file
+              </span>
+              <span className="mt-2 text-zinc-700 md:text-center">
+                We&apos;ll process your file and make it ready for you to chat
+                with.
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4 md:items-center">
+              <span className="text-sm font-semibold text-indigo-600">
+                Step 3
+              </span>
+              <span className="text-xl font-semibold ">
+                Start asking questions
+              </span>
+              <span className="mt-2 text-zinc-700 md:text-center">
+                It&apos;s that simple. Try out Quill today - it really takes
+                less than a minute.
+              </span>
+            </div>
+          </li>
+        </ol>
+
+        <div className="mx-auto max-w-6xl px-12 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24">
+            <div className="-m-2 rounded-xl bg-gray-900/25 p-0.5 md:p-1 ring-1 ring-inset ring-gray-900/20 lg:-m-4  ">
+              <Image
+                src="/file-upload-preview.jpg"
+                alt="uploading preview"
+                width={1419}
+                height={732}
+                quality={100}
+                className="rounded-[10px] md:rounded-[9px]  bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
