d9a7bdc7ced2862d62698ce668cbf352208ee5d2
Himansu <himansuthapa2059@gmail.com>
2024-10-18 23:08:53 +0545

    code refactored

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { MoveRight } from 'lucide-react';
 import { buttonVariants } from '@/components/ui/button';
 import Image from 'next/image';
+import { steps } from '@/constants';
 
 export default function Home() {
   return (
@@ -101,59 +102,45 @@ export default function Home() {
         </div>
 
         {/* steps */}
+
         <ol className="my-8 space-y-4 p-4 md:px-8 md:flex md:space-x-12 md:space-y-0">
-          <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4 md:items-center">
-              <span className="text-sm font-semibold text-indigo-600">
-                Step 1
-              </span>
-              <span className="text-xl font-semibold ">
-                Sign up for an account
-              </span>
-              <span className="mt-2 text-zinc-700 md:text-center">
-                Either starting out with a free plan or choose our{' '}
-                <Link
-                  href="/pricing"
-                  className="text-indigo-700 underline underline-offset-2"
-                >
-                  pro plan
-                </Link>
-                .
-              </span>
-            </div>
-          </li>
-          <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4 md:items-center">
-              <span className="text-sm font-semibold text-indigo-600">
-                Step 2
-              </span>
-              <span className="text-xl font-semibold ">
-                Upload your PDF file
-              </span>
-              <span className="mt-2 text-zinc-700 md:text-center">
-                We&apos;ll process your file and make it ready for you to chat
-                with.
-              </span>
-            </div>
-          </li>
-          <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4 md:items-center">
-              <span className="text-sm font-semibold text-indigo-600">
-                Step 3
-              </span>
-              <span className="text-xl font-semibold ">
-                Start asking questions
-              </span>
-              <span className="mt-2 text-zinc-700 md:text-center">
-                It&apos;s that simple. Try out Quill today - it really takes
-                less than a minute.
-              </span>
-            </div>
-          </li>
+          {steps.map((step) => (
+            <li className="md:flex-1" key={step.number}>
+              <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4 lg:items-center">
+                <span className="text-sm font-semibold text-indigo-600">
+                  Step {step.number}
+                </span>
+                <span className="text-xl font-semibold ">{step.title}</span>
+                <span className="mt-2 text-zinc-700 lg:text-center">
+                  {step.description}{' '}
+                  {step.linkHref && step.linkLabel && (
+                    <Link
+                      href={step.linkHref}
+                      className="text-indigo-700 underline underline-offset-2"
+                    >
+                      {step.linkLabel}
+                    </Link>
+                  )}
+                </span>
+              </div>
+            </li>
+          ))}
         </ol>
 
-        <div className="mx-auto max-w-6xl px-12 lg:px-8">
-          <div className="mt-16 flow-root sm:mt-24">
+        <div className="mx-auto max-w-6xl px-12 lg:px-8 relative">
+          <div className="mt-16 flow-root sm:mt-24 ">
+            <div
+              aria-hidden="true"
+              className="pointer-events-none absolute inset-x-0 -top-20 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-60"
+            >
+              <div
+                style={{
+                  clipPath:
+                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                }}
+                className="relative right-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] translate-x-1/3 rotate-[65deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:right-[calc(50%-36rem)] sm:w-[72.1875rem]"
+              />
+            </div>
             <div className="-m-2 rounded-xl bg-gray-900/25 p-0.5 md:p-1 ring-1 ring-inset ring-gray-900/20 lg:-m-4  ">
               <Image
                 src="/file-upload-preview.jpg"
---
+++ constants/index.ts
@@ -0,0 +1,19 @@
+export const steps = [
+  {
+    number: '1',
+    title: 'Sign up an account',
+    description: "We'll process your file & make it ready for you to chat",
+    linkLabel: 'pro plan',
+    linkHref: '/pricing',
+  },
+  {
+    number: '2',
+    title: 'Upload your PDF file',
+    description: "We'll process your file and make it ready for you to chat",
+  },
+  {
+    number: '3',
+    title: 'Start asking questions',
+    description: 'Try out AskMyPDF today, It really takes less than a minute.',
+  },
+];
