cd5c1276f88b2a0b62ebadd4c490f1f8e64c2b35
Himansu <himansuthapa2059@gmail.com>
2024-10-22 22:13:47 +0545

    feat: delete files

---
+++ app/api/files/[fileId]/route.ts
@@ -0,0 +1,40 @@
+import prisma from '@/lib/db';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { NextResponse } from 'next/server';
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { fileId: string } }
+) {
+  try {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    const { fileId } = params;
+
+    if (!user.id || !user.email) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const file = await prisma.file.findFirst({
+      where: {
+        id: fileId,
+        userId: user.id,
+      },
+    });
+
+    if (!file) return new NextResponse('not-found', { status: 404 });
+
+    await prisma.file.delete({
+      where: {
+        id: fileId,
+        userId: user.id,
+      },
+    });
+
+    return NextResponse.json(file);
+  } catch (err: any) {
+    console.log('[deleting-files]', err);
+    throw new NextResponse('Internal server error', { status: 500 });
+  }
+}
--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,7 +2,14 @@
 import React, { useEffect, useState } from 'react';
 import UploadButton from './UploadButton';
 import Skeleton from 'react-loading-skeleton';
-import { Ghost, CircleX, Plus, MessageSquare, Trash } from 'lucide-react';
+import {
+  Ghost,
+  CircleX,
+  Plus,
+  MessageSquare,
+  Trash,
+  Loader2,
+} from 'lucide-react';
 import axios from 'axios';
 import { FileArray } from '@/lib/types';
 import { format } from 'date-fns';
@@ -13,6 +20,9 @@ const Dashboard = () => {
   const [files, setFiles] = useState<FileArray>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<null | string>(null);
+  const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<
+    string | null
+  >(null);
 
   useEffect(() => {
     const fetchFiles = async () => {
@@ -30,6 +40,19 @@ const Dashboard = () => {
     };
     fetchFiles();
   }, []);
+
+  const handleDelete = async (fileId: string) => {
+    try {
+      setCurrentlyDeletingFile(fileId);
+      setError(null);
+      await axios.delete(`/api/files/${fileId}`);
+      setFiles(files.filter((file) => file.id !== fileId)); // Remove deleted file from state
+    } catch (err) {
+      setError('Something went wrong during deletion!');
+    } finally {
+      setCurrentlyDeletingFile(null);
+    }
+  };
 
   return (
     <main className="max-w-7xl mx-auto md:p-10 ">
@@ -54,7 +77,64 @@ const Dashboard = () => {
       {/* Only render files if not loading, no error, and files exist */}
       {!isLoading && !error ? (
         files.length > 0 ? (
-          <div>all files</div>
+          <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
+            {files
+              .sort(
+                (a, b) =>
+                  new Date(b.createdAt).getTime() -
+                  new Date(a.createdAt).getTime()
+              )
+              .map((file) => (
+                <li
+                  key={file.id}
+                  className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg"
+                >
+                  <Link
+                    href={`/dashboard/${file.id}`}
+                    className="flex flex-col gap-2"
+                  >
+                    <div className="pt-6 px-6 w-full flex items-center justify-between space-x-6">
+                      <div className="h-10 w-10 shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+                      <div className="flex-1 truncate">
+                        <div className="flex items-center space-x-3">
+                          <h3 className="truncate text-lg font-medium text-zinc-900">
+                            {file.name}
+                          </h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+
+                  <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500">
+                    <div className="flex items-center gap-2">
+                      <Plus className="h-4 w-4" />
+                      <span>
+                        {format(new Date(file.createdAt), 'dd MMM yyyy')}
+                      </span>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                      <MessageSquare className="h-4 w-4" />
+                      <span>mocked</span>
+                    </div>
+
+                    <Button
+                      onClick={() => handleDelete(file.id)}
+                      variant="destructive"
+                      size="sm"
+                      className="w-full text-zinc-500 bg-transparent hover:bg-rose-100 border border-rose-300"
+                    >
+                      {currentlyDeletingFile === file.id ? (
+                        <Loader2 className="h-4 w-4 animate-spin" />
+                      ) : (
+                        <Trash className="h-4 w-4" stroke="red" />
+                      )}{' '}
+                      <span>Delete</span>
+                    </Button>
+                  </div>
+                </li>
+              ))}
+          </ul>
         ) : (
           <div className="mt-16 flex flex-col items-center gap-2">
             <Ghost className="h-8 w-8 text-zinc-800" />
