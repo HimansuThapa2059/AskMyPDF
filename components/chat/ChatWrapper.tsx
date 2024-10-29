49220a060c0f40139e78c309b8c51b16969f8c57
Himansu <himansuthapa2059@gmail.com>
2024-10-29 20:39:15 +0545

    add: chatInput box

--- components/chat/ChatWrapper.tsx
+++ components/chat/ChatWrapper.tsx
@@ -63,23 +63,23 @@ const ChatWrapper = ({ fileId }: { fileId: string }) => {
     );
   }
 
-  // if (data?.status === "PROCESSING") {
-  //   return (
-  //     <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-zinc-200 bg-zinc-50">
-  //       <div className="mb-28 flex flex-1 flex-col items-center justify-center">
-  //         <div className="flex flex-col items-center gap-2">
-  //           <Loader2 className="h-8 w-8 animate-spin text-indigo-500" />
-  //           <h3 className="text-xl font-semibold">Processing...</h3>
-  //           <p className="text-sm text-zinc-500">
-  //             This won&apos;t take too long.
-  //           </p>
-  //         </div>
-  //       </div>
+  if (data?.status === "PROCESSING") {
+    return (
+      <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-zinc-200 bg-zinc-50">
+        <div className="mb-28 flex flex-1 flex-col items-center justify-center">
+          <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 animate-spin text-indigo-500" />
+            <h3 className="text-xl font-semibold">Processing...</h3>
+            <p className="text-sm text-zinc-500">
+              This won&apos;t take too long.
+            </p>
+          </div>
+        </div>
 
-  //       <ChatInput isDisabled />
-  //     </div>
-  //   );
-  // }
+        <ChatInput isDisabled />
+      </div>
+    );
+  }
 
   if (data?.status === "FAILED") {
     return (
